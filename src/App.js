fdd9c1f006ecf4f8489e6665c76443d04197f5a7
Zeronimo <bpscokr003@naver.com>
2024-05-23 15:21:17 +0900

    test1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
+function rotate(){
+ var container = document.querySelector('.container');
+ container.addEventListener('mousemove', function(){
+ })
+}
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div class ='container'>
+  <div class = 'card'></div>
+</div>
+
   );
 }
 
